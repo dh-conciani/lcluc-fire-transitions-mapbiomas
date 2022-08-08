32d0449bd24c1989263115bf746763e296a54acb
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2022-08-08 09:56:17 -0300

    Create 01_getData.js

---
+++ changes/01_getData.js
@@ -0,0 +1,36 @@
+// get fire frequency (col. 1) for the entire period (1985-2020)
+var fire_freq = ee.Image('projects/mapbiomas-workspace/public/collection6/mapbiomas-fire-collection1-fire-frequency-1')
+      .select('fire_frequency_1985_2020')
+      .divide(100).int()
+      .rename(['fire_freq']);
+
+// get land cover and land use (col. 7) 
+var collection = ee.Image('projects/mapbiomas-workspace/public/collection7/mapbiomas_collection70_integration_v2')
+      .slice(0,36);    // remove 2021
+
+// compute n changes
+var n_changes = collection.reduce(ee.Reducer.countRuns()).subtract(1)
+      .rename(['n_changes']);
+
+// compute n classes
+var n_classes = collection.reduce(ee.Reducer.countDistinctNonNull())
+      .rename(['n_classes']);
+
+// get mode land use and land cover
+var mode = collection.reduce(ee.Reducer.mode())
+      .rename(['mode']);
+
+// get pixel area (in hectares)
+var pixel_area = ee.Image.pixelArea().divide(10000)
+      .rename(['area_ha']);
+
+// stack data
+var data = fire_freq.addBands(
+  n_changes).addBands(
+    n_classes).addBands(
+      mode).addBands(
+        pixel_area).aside(
+          print);
+
+// plot
+Map.addLayer(data, {}, 'data');
